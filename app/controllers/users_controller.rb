0c752bd8bfcb47c1517bff1a2e4e225c8415c3a5
Ilia Gavrilov <sandik@mail.ru>
2015-01-13 18:12:44 +0400

    Before friendly redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -6,6 +9,21 @@ class UsersController < ApplicationController
   def new
 	@user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Successfuly updated info"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   
   def create
     @user = User.new(user_params)
@@ -24,5 +42,23 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-	
+
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        flash[:notice] = "Please sign in."
+        redirect_to signin_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        flash[:notice] = "Please sign in."
+        redirect_to(root_url) 
+      end
+    end	
+
 end
